8f887071637ffc3ca74a285e178c90aaa73c5cab
JOSH <onyemajoshua892@gmail.com>
2022-03-17 17:18:21 +0100

    save

--- js/script.js
+++ js/script.js
@@ -122,22 +122,36 @@ window.addEventListener("scroll", () => {
 topBtn.addEventListener("click", () => {
     document.documentElement.scrollTop = 0;
 });
-//////////////////////////////////////////
 
-const toggleBtn = document.querySelector(".dark-mode");
+//////////////////////////////////////////
+////// Dark Mode
+const toggleBtn = document.querySelector(".dark-mode"); //set the dark mode button to a variable
 const articlesContainer = document.querySelector("html");
 
+let logoImage = document.getElementById("logo");
 toggleBtn.addEventListener("click", () => {
     document.documentElement.classList.toggle("dark");
+
+    let initialMode = localStorage.getItem("colorMode");
+    if (initialMode && initialMode === "dark") {
+        localStorage.setItem("colorMode", "light");
+        logoImage.setAttribute("src", "images/logo.png");
+    } else {
+        localStorage.setItem("colorMode", "dark");
+        logoImage.setAttribute("src", "images/logo1.png");
+    }
 });
 
-// const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
-// if (prefersDarkScheme.matches) {
-//     document.body.classList.add("dark");
-//     toggleBtn.addEventListener("click", () => {
-//         document.documentElement.classList.toggle("dark" || "light");
-//     });
+if (prefersDarkScheme.matches) {
+    document.documentElement.classList.toggle("dark");
+    localStorage.setItem("colorMode", "dark");
+    logoImage.setAttribute("src", "images/logo1.png");
+}
+// toggleBtn.addEventListener("toggle", () => {
+//     document.documentElement.classList.toggle("light");
+// });
 // } else {
 //     document.body.classList.remove("dark");
 // }
@@ -183,52 +197,52 @@ toggleBtn.addEventListener("click", () => {
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 
 /*
-.no-flexbox-gap .main-nav-list li:not(:last-child) {
-  margin-right: 4.8rem;
-}
+    .no-flexbox-gap .main-nav-list li:not(:last-child) {
+      margin-right: 4.8rem;
+    }
 
-.no-flexbox-gap .list-item:not(:last-child) {
-  margin-bottom: 1.6rem;
-}
+    .no-flexbox-gap .list-item:not(:last-child) {
+      margin-bottom: 1.6rem;
+    }
 
-.no-flexbox-gap .list-icon:not(:last-child) {
-  margin-right: 1.6rem;
-}
+    .no-flexbox-gap .list-icon:not(:last-child) {
+      margin-right: 1.6rem;
+    }
 
-.no-flexbox-gap .delivered-faces {
-  margin-right: 1.6rem;
-}
+    .no-flexbox-gap .delivered-faces {
+      margin-right: 1.6rem;
+    }
 
-.no-flexbox-gap .meal-attribute:not(:last-child) {
-  margin-bottom: 2rem;
-}
+    .no-flexbox-gap .meal-attribute:not(:last-child) {
+      margin-bottom: 2rem;
+    }
 
-.no-flexbox-gap .meal-icon {
-  margin-right: 1.6rem;
-}
+    .no-flexbox-gap .meal-icon {
+      margin-right: 1.6rem;
+    }
 
-.no-flexbox-gap .footer-row div:not(:last-child) {
-  margin-right: 6.4rem;
-}
+    .no-flexbox-gap .footer-row div:not(:last-child) {
+      margin-right: 6.4rem;
+    }
 
-.no-flexbox-gap .social-links li:not(:last-child) {
-  margin-right: 2.4rem;
-}
+    .no-flexbox-gap .social-links li:not(:last-child) {
+      margin-right: 2.4rem;
+    }
 
-.no-flexbox-gap .footer-nav li:not(:last-child) {
-  margin-bottom: 2.4rem;
-}
+    .no-flexbox-gap .footer-nav li:not(:last-child) {
+      margin-bottom: 2.4rem;
+    }
 
-@media (max-width: 75em) {
-  .no-flexbox-gap .main-nav-list li:not(:last-child) {
-    margin-right: 3.2rem;
-  }
-}
+    @media (max-width: 75em) {
+      .no-flexbox-gap .main-nav-list li:not(:last-child) {
+        margin-right: 3.2rem;
+      }
+    }
 
-@media (max-width: 59em) {
-  .no-flexbox-gap .main-nav-list li:not(:last-child) {
-    margin-right: 0;
-    margin-bottom: 4.8rem;
-  }
-}
-*/
+    @media (max-width: 59em) {
+      .no-flexbox-gap .main-nav-list li:not(:last-child) {
+        margin-right: 0;
+        margin-bottom: 4.8rem;
+      }
+    }
+    */
